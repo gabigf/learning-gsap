aeba07af66297bf45f2ce65c6486be750a867912
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-09-04 23:42:29 -0400

    Add restart and reverse buttons

--- CreativeCodingClub/GSAP3Express/public/scripts/delay-repeat.js
+++ CreativeCodingClub/GSAP3Express/public/scripts/delay-repeat.js
@@ -2,6 +2,15 @@ let greenFred = gsap.to('.green', {
   duration: 2,
   x: 600,
   repeat: 1,
-  yoyo: true,
 });
 let pinkFred = gsap.to('.pink', { duration: 2, x: 600, delay: 2 });
+
+document.querySelector('.restart-button').onclick = () => {
+  greenFred.restart();
+  pinkFred.restart();
+};
+
+document.querySelector('.reverse-button').onclick = () => {
+  greenFred.reverse();
+  pinkFred.reverse();
+};
