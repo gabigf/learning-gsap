bce1bb7f1fd303d2ce6cdaf8bba3e6466afdbef6
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-09-04 23:54:58 -0400

    Add ease markup and gsap logic

--- CreativeCodingClub/GSAP3Express/public/scripts/ease.js
+++ CreativeCodingClub/GSAP3Express/public/scripts/ease.js
@@ -1,11 +1,13 @@
 let greenFred = gsap.to('.green', {
   duration: 2,
   x: 600,
-  repeat: 1,
+  ease: 'bounce.inOut',
 });
-// Setting repeat to -1 will make the animation run infinitely
-
-let pinkFred = gsap.to('.pink', { duration: 2, x: 600, delay: 2 });
+let pinkFred = gsap.to('.pink', {
+  duration: 2,
+  x: 600,
+  ease: 'linear',
+});
 
 document.querySelector('.restart-button').onclick = () => {
   greenFred.restart();
