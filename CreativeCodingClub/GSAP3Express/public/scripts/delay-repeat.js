941f48908c04dfbe233fb4894a2cdd7e1881e965
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-09-04 23:38:00 -0400

    Add repeat params

--- CreativeCodingClub/GSAP3Express/public/scripts/delay-repeat.js
+++ CreativeCodingClub/GSAP3Express/public/scripts/delay-repeat.js
@@ -1,2 +1,7 @@
-let greenFred = gsap.to('.green', { duration: 2, x: 600 });
+let greenFred = gsap.to('.green', {
+  duration: 2,
+  x: 600,
+  repeat: 1,
+  yoyo: true,
+});
 let pinkFred = gsap.to('.pink', { duration: 2, x: 600, delay: 2 });
