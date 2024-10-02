7a51ff9c1733ee9210502f111d85a355abbdac04
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-10-02 17:04:36 -0400

    Add styling to pages

--- CreativeCodingClub/GSAP3Express/public/scripts/from-to.js
+++ CreativeCodingClub/GSAP3Express/public/scripts/from-to.js
@@ -1,7 +1,7 @@
 let fred = gsap.fromTo(
   '.fred',
-  { x: 700, y: 400 },
-  { x: 300, y: 200, duration: 3, scale: 2 }
+  { x: 700, y: -100 },
+  { x: 300, y: 100, duration: 3, scale: 2 }
 );
 
 document.querySelector('.restart-button').onclick = () => {
