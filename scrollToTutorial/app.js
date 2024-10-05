fa668d600e958f831e6191ed992f0a2eac9cee90
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-10-05 18:40:19 -0400

    Experiment with inifinite scroll animation

--- scrollToTutorial/app.js
+++ scrollToTutorial/app.js
@@ -1,30 +1,62 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.create({
-  animation: gsap.from(".logo", {
-    y: "50vh",
-    scale: 6,
-    yPercent: -50
-  }),
-  scrub: true,
-  trigger: ".content",
-  start: "top bottom",
-  endTrigger: ".content",
-  end: "top center"
-})
+  ScrollTrigger.create({
+    animation: gsap.from('.logo', {
+      y: '50vh',
+      scale: 6,
+      yPercent: -50,
+    }),
+    scrub: true,
+    trigger: '.content',
+    start: 'top bottom',
+    endTrigger: '.content',
+    end: 'top center',
+  });
 
-ScrollTrigger.create({
-  animation: gsap.from(".landing-description", {
-    y: "50vh",
-    opacity: 1,
-    yPercent: -50
-  }),
-  scrub: true,
-  trigger: ".content",
-  start: "top bottom",
-  endTrigger: ".content",
-  end: "top center"
-})
-
+  ScrollTrigger.create({
+    animation: gsap.from('.landing-description', {
+      y: '50vh',
+      opacity: 1,
+      yPercent: -50,
+    }),
+    scrub: true,
+    trigger: '.content',
+    start: 'top bottom',
+    endTrigger: '.content',
+    end: 'top center',
+  });
 });
+
+// SCROLLER LOGIC - infinite scroll
+
+const scrollers = document.querySelectorAll('.scroller');
+
+if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute('data-animated', true);
+
+    const scrollerInner = scroller.querySelector('.scroller_inner');
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute('aria-hidden', true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+function addHoverEffect(item, scroller) {
+  scroller.setAttribute('paused', true);
+  item.setAttribute('is-hovered', true);
+}
+
+function removeHoverEffect(item, scoller) {
+  scroller.setAttribute('paused', false);
+  item.setAttribute('is-hovered', false);
+}
